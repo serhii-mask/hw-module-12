00ec926c8ca1c7222b0b7d49c9a28acb45b9bcc0
Serhii <serhii.mask@gmail.com>
2023-06-12 18:29:57 +0300

    fix: fizzBuzz

--- src/main/java/task2/FizzBuzz.java
+++ src/main/java/task2/FizzBuzz.java
@@ -1,105 +1,123 @@
 package task2;
 
-class FizzBuzz {
-    private final int n;
-    private volatile int current;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
-    public FizzBuzz() {
-        this.n = 15;
-        this.current = 1;
-    }
+class FizzBuzz {
+    private int n;
+    private BlockingQueue<String> queue;
+    private Object lock;
+    private int current;
 
     public FizzBuzz(int n) {
         this.n = n;
+        this.queue = new LinkedBlockingQueue<>();
+        this.lock = new Object();
         this.current = 1;
     }
 
-    public synchronized void fizzBuzz() {
-        while (current <= n) {
-
-            if (current % 3 == 0 && current % 5 == 0) {
-                System.out.println("fizzbuzz");
-                current++;
-                notifyAll();
-            } else {
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+    public void fizz() {
+        while (true) {
+            synchronized (lock) {
+                while (current <= n && (current % 3 != 0 || current % 5 == 0)) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-        }
-    }
-
-    public synchronized void fizz() {
-        while (current <= n) {
-
-            if (current % 3 == 0 && current % 5 != 0) {
+                if (current > n) {
+                    break;
+                }
+                queue.add("fizz");
                 System.out.println("fizz");
                 current++;
-                notifyAll();
-            } else {
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                lock.notifyAll();
             }
         }
     }
 
-    public synchronized void buzz() {
-        while (current <= n) {
-
-            if (current % 5 == 0 && current % 3 != 0) {
+    public void buzz() {
+        while (true) {
+            synchronized (lock) {
+                while (current <= n && (current % 5 != 0 || current % 3 == 0)) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (current > n) {
+                    break;
+                }
+                queue.add("buzz");
                 System.out.println("buzz");
                 current++;
-                notifyAll();
-            } else {
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                lock.notifyAll();
             }
         }
     }
 
-    public synchronized void number() {
-        while (current <= n) {
+    public void fizzBuzz() {
+        while (true) {
+            synchronized (lock) {
+                while (current <= n && (current % 3 != 0 || current % 5 != 0)) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (current > n) {
+                    break;
+                }
+                queue.add("fizzbuzz");
+                System.out.println("fizzbuzz");
+                current++;
+                lock.notifyAll();
+            }
+        }
+    }
 
-            if (current % 3 != 0 && current % 5 != 0) {
+    public void number() {
+        while (true) {
+            synchronized (lock) {
+                while (current <= n && (current % 3 == 0 || current % 5 == 0)) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (current > n) {
+                    break;
+                }
                 System.out.println(current);
                 current++;
-                notifyAll();
-            } else {
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                lock.notifyAll();
             }
         }
     }
 
     public static void main(String[] args) {
-        FizzBuzz fizzBuzz = new FizzBuzz(15);
+        int n = 15;
+        FizzBuzz fizzBuzz = new FizzBuzz(n);
 
         Thread threadA = new Thread(fizzBuzz::fizz);
         Thread threadB = new Thread(fizzBuzz::buzz);
         Thread threadC = new Thread(fizzBuzz::fizzBuzz);
         Thread threadD = new Thread(fizzBuzz::number);
 
+        threadD.start();
         threadA.start();
         threadB.start();
         threadC.start();
-        threadD.start();
 
         try {
+            threadD.join();
             threadA.join();
             threadB.join();
             threadC.join();
-            threadD.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
